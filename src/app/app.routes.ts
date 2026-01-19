464483cdacf71f291dfabb786b657224aa704de7
Jude Boachie <jude.boachie@amalitech.com>
2026-01-19 14:22:10 +0000

    feat: implement cart and product list navigation with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Cart } from './component/cart/cart';
+import { ProductList } from './component/product-list/product-list';
+import { NotFound } from './component/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'products',
+  //   pathMatch: 'full',
+  // },
+  {
+    path: 'products',
+    component: ProductList,
+  },
+  {
+    path: 'cart',
+    component: Cart,
+  },
+  {
+    path: '**',
+    component: NotFound,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
-import { Component, signal } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
-import { ProductList } from "./component/product-list/product-list";
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductList],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('my-angular-app')
+  protected readonly title = signal('my-angular-app');
+  router = inject(Router);
+
+  handleCartNavigation() {
+    this.router.navigateByUrl('cart');
+  }
+
+  handleProductListNavigation() {
+    this.router.navigateByUrl('products');
+  }
 }
